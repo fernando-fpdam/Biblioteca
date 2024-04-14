b1d8e300d0db8fa56daf76cb080d9d7f92f4eff3
fernando-fpdam <159696552+fernando-fpdam@users.noreply.github.com>
2024-04-14 11:25:08 +0200

    Menu y switch_case establecido

--- libreria_fernando/src/main/java/libreria_fernando/main.java
+++ libreria_fernando/src/main/java/libreria_fernando/main.java
@@ -4,6 +4,7 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.SessionFactory;
 
 import java.util.List;
+import java.util.Scanner;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
@@ -11,6 +12,8 @@ import org.hibernate.Transaction;
 public class main {
 
 	public static void main(String[] args) {
+		
+	// CONFIGURACIÓN DEL PROGRAMA
 		System.out.println("INICO DEL PROGRAMA...");
 		// Para indicar que queremos usar Hibernate definimos las interfaces.
 		
@@ -22,6 +25,58 @@ public class main {
 		Session session = sessionFactory.openSession();
 		
 		System.out.println("CONFIGURACIÓN REALIZADA");
+		
+	// PROGRAMA PRINCIPAL.
+		int opcion = 0;
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		
+		do {
+			System.out.println("--------------------------------");
+			System.out.println("\t\tBIBLIOTECA");
+			System.out.println("--------------------------------");
+			System.out.println("1- Insertar Libro.");
+			System.out.println("2- Insertar Lector.");
+			System.out.println("3- Listado de Libros.");
+			System.out.println("4- Listado de Lectores.");
+			System.out.println("5- Ver Libro por ID.");
+			System.out.println("6- Ver Lector por ID.");
+			System.out.println("7- Salir.");
+			System.out.println("--------------------------------");
+			System.out.println("selecciona una opción:");
+			opcion = sc.nextInt();
+			
+			switch (opcion) {
+			case 1: // 1. Insertar Libro.
+				
+				break;	
+			case 2: // 2. Insertar Lector.
+				
+				break;
+			case 3: // 3. Listado de Libros.
+				
+				break;
+			case 4: // 4. Listado de Lectores.
+	
+				break;
+			
+			case 5: // 5. Ver Libro por ID.
+				
+				break;
+			case 6: // 6. Ver Lector por ID.
+				
+				break;
+				
+			default:
+				System.out.println("Opción incorrecta, inténtalo de nuevo...");
+				break;
+			}
+			
+		} while (opcion !=7);
+		
+		
 		
 		
 		session.close();
